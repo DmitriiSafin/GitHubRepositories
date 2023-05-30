8cce0ee1c9d42b9a33f0c87cffcdceaf36265c88
Dmitry Safin <developersafin@gmail.com>
2023-05-30 15:56:04 +0500

    start project

--- GitHubRepositories/AuthViewController.swift
+++ GitHubRepositories/AuthViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AuthViewController.swift
 //  GitHubRepositories
 //
 //  Created by Дмитрий on 30.05.2023.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AuthViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
     }
 
 
